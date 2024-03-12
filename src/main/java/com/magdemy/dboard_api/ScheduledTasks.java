6893edb060dc5c979d318446124da172b82cdeb0
Parzivial96 <yogesh.pdy4@gmail.com>
2024-03-12 22:46:01 +0530

    Finally added Marquee and Todo

--- src/main/java/com/magdemy/dboard_api/ScheduledTasks.java
+++ src/main/java/com/magdemy/dboard_api/ScheduledTasks.java
@@ -1,6 +1,8 @@
 package com.magdemy.dboard_api;
 
+import com.magdemy.dboard_api.service.MarqueeService;
 import com.magdemy.dboard_api.service.NoticeService;
+import com.magdemy.dboard_api.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,8 +13,24 @@ public class ScheduledTasks {
     @Autowired
     private NoticeService noticeService;
 
+    @Autowired
+    private TodoService todoService;
+
+    @Autowired
+    private MarqueeService marqueeService;
+
     @Scheduled(fixedRate = 24 * 60 * 60 * 1000) // Run every 24 hours
     public void deleteExpiredImages() {
         noticeService.deleteExpiredImages();
+    }
+
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000) // Run every 24 hours
+    public void deleteExpiredTodos() {
+        todoService.deleteExpiredTodos();
+    }
+
+    @Scheduled(fixedRate = 24 * 60 * 60 * 1000) // Run every 24 hours
+    public void deleteExpiredMarquee() {
+        marqueeService.deleteExpiredMarquees();
     }
 }
--- src/main/java/com/magdemy/dboard_api/controller/NoticeController.java
+++ src/main/java/com/magdemy/dboard_api/controller/NoticeController.java
@@ -1,7 +1,11 @@
 package com.magdemy.dboard_api.controller;
 
+import com.magdemy.dboard_api.model.Marquee;
 import com.magdemy.dboard_api.model.Notice;
+import com.magdemy.dboard_api.model.Todo;
+import com.magdemy.dboard_api.service.MarqueeService;
 import com.magdemy.dboard_api.service.NoticeService;
+import com.magdemy.dboard_api.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +18,12 @@ import java.util.List;
 public class NoticeController {
     @Autowired
     private NoticeService noticeService;
+
+    @Autowired
+    private TodoService todoService;
+
+    @Autowired
+    private MarqueeService marqueeService;
 
     @GetMapping("/getAllNotice")
     public List<Notice> getAllNotice(){
@@ -29,5 +39,38 @@ public class NoticeController {
         catch (Exception e){
             return "Upload Error";
         }
+    }
+
+    @PostMapping("/uploadTodo")
+    public String uploadTodo(@RequestBody Todo todo){
+        try {
+            todoService.saveTodo(todo.getTextData(),todo.getExpiryDate());
+            return "Uploaded Successfully";
+        }
+        catch (Exception e){
+            return "Upload Error";
+        }
+    }
+
+    @GetMapping("/getAllTodo")
+    public List<Todo> getAllTodo(){
+        return todoService.getAllTodo();
+    }
+
+
+    @PostMapping("/uploadMarquee")
+    public String uploadMarquee(@RequestBody Marquee marquee){
+        try {
+            marqueeService.saveMarquee(marquee.getMarqueeData(),marquee.getExpiryDate());
+            return "Uploaded Successfully";
+        }
+        catch (Exception e){
+            return "Upload Error";
+        }
+    }
+
+    @GetMapping("/getAllMarquee")
+    public List<Marquee> getAllMarquee(){
+        return marqueeService.getAllMarquee();
     }
 }
